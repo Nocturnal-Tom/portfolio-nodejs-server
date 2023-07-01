dec05d7fe2c8ae220f526cf2f71ca47620cb229a
Tommy <tommyreade9608@gmail.com>
2023-07-01 14:28:26 +0000

    Added server file

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const PORT = "40281"
 
 const app = express();
-
+const blogDir = process.env.BLOGDIR;
+console.log("blogDir: ", blogDir);
+console.log("www dir: ", process.env.WWWDIR);
 
 app.get("/api/blog*", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
