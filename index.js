b242e21f649c6460d77cbba9a2a51b5a10082a85
Tommy <root@li720-169.members.linode.com>
2023-07-06 07:45:45 +0000

    Added capability to save messages submitted from the portfolio site

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 
 const PORT = "40281"
 
@@ -6,6 +7,9 @@ const app = express();
 const blogDir = process.env.BLOGDIR;
 console.log("blogDir: ", blogDir);
 console.log("www dir: ", process.env.WWWDIR);
+
+
+app.use(express.json({type: "text/plain"}));
 
 app.get("/api/blog*", (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -19,5 +23,12 @@ app.get("/api/blog*", (req, res) => {
         ]
     }));
 })
+
+app.post("/api/contactForm", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log(req.body);
+	fs.appendFile("contacts.txt", JSON.stringify(req.body).concat("\n"), (err) => {});
+	res.send("");
+});
 
 app.listen(PORT);
