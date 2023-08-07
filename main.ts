c7bae1005f8029dc906e5289a3590632b7b39c9e
Tommy <tommyreade9608@gmail.com>
2023-08-07 08:31:01 +0100

    Removed javascript and pgi, switched to deno, now using prisma for database interaction

---
+++ main.ts
@@ -0,0 +1,12 @@
+import prisma from "prisma"; 
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+}
+prisma.
+console.log(prisma)
--- test.js
+++
@@ -1,1 +0,0 @@
-console.log(`'a' === 0x61: ${'a'.charCodeAt(0) === 97}`);
